bf07009e33aa78bb54089bf6b9cd9c3a23982724
guoruize <yimao009@qq.com>
2017-05-18 10:02:42 +0800

    Initial Commit

---
+++ BookCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BookCollection
+//
+//  Created by Kwok_Rui on 2017/5/18.
+//  Copyright © 2017年 Netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BookCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BookCollection
+//
+//  Created by Kwok_Rui on 2017/5/18.
+//  Copyright © 2017年 Netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
